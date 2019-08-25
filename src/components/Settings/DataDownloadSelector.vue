a5ca2acea899093f2cc4389a11097c5b90b5e6dd
Joshua Castor <joshua.castor@joshuacastor.me>
2019-08-24 23:19:29 -0500

    Added initial layout for data downlaod area

--- src/components/Settings/DataDownloadSelector.vue
+++ src/components/Settings/DataDownloadSelector.vue
@@ -1,7 +1,25 @@
 <template>
-	<ul>
-		<li v-for="name in allDataEntriesByName" :key="name">
-			<span>{{ name }}</span>
+	<ul class="data-download-selector-list">
+		<li v-for="pair in dataNameKeyPairs" :key="pair.name">
+			<v-layout row wrap align-baseline>
+				<v-flex xs12 sm3>
+					<v-label>
+						{{ pair.name }}
+					</v-label>
+				</v-flex>
+				<v-flex
+					v-for="server in servers"
+					:key="server"
+					xs4 sm3
+				>
+					<v-checkbox
+						v-model="pairsToDownload"
+						:value="createPairKey(pair.key, server)"
+						:label="server"
+						:aria-label="`Download ${server} data for ${pair.name}`"
+					/>
+				</v-flex>
+			</v-layout>
 		</li>
 	</ul>
 </template>
@@ -11,13 +29,29 @@ import { DATA_MAPPING, SERVERS } from '@/utilities/constants';
 
 export default {
 	computed: {
-		allDataEntriesByName () {
+		dataNameKeyPairs () {
 			return Object.keys(DATA_MAPPING)
-				.map(key => DATA_MAPPING[key].name);
+				.map(key => ({ key, name: DATA_MAPPING[key].name }));
 		},
 		servers () {
 			return SERVERS.slice();
 		},
 	},
+	data () {
+		return {
+			pairsToDownload: [],
+		};
+	},
+	methods: {
+		createPairKey (key, server) {
+			return `${key}-${server}`;
+		},
+	},
 };
 </script>
+
+<style lang="scss">
+ul.data-download-selector-list {
+	list-style: none;
+}
+</style>
